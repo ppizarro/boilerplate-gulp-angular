99d902e71b2e7805d194ff71a801187164636067
Paulo Pizarro <paulo.pizarro@gmail.com>
2014-10-20 20:18:13 -0200

    Creating an AngularJS $templateCache

--- gulp/build.js
+++ gulp/build.js
@@ -6,15 +6,27 @@ var pkg = require('../package.json');
 
 gulp.task('help', plug.taskListing);
 
+gulp.task('templatecache', function() {
+    return gulp
+        .src(pkg.paths.htmltemplates)
+        .pipe(plug.angularTemplatecache('templates.js', {
+            module: 'app.core',
+            standalone: false,
+            root: 'app/'
+        }))
+        .pipe(gulp.dest(pkg.paths.build));
+});
+
 gulp.task('analyze', function () {
-  return gulp.src(pkg.paths.js)
-    .pipe(plug.jshint('./.jshintrc'))
-    .pipe(plug.jshint.reporter('jshint-stylish'))
-    .pipe(plug.jscs('./.jscsrc'));
+    return gulp.src(pkg.paths.js)
+        .pipe(plug.jshint('./.jshintrc'))
+        .pipe(plug.jshint.reporter('jshint-stylish'))
+        .pipe(plug.jscs('./.jscsrc'));
 });
 
 gulp.task('clean', function () {
-  return gulp.src([pkg.paths.tmp, pkg.paths.build], { read: false }).pipe(plug.rimraf());
+    return gulp.src([pkg.paths.tmp, pkg.paths.build], { read: false })
+        .pipe(plug.rimraf());
 });
 
 gulp.task('build', ['analyze']);
